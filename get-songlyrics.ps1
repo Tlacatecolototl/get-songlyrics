eb9bcf0b7cf78542edb79453a2d82622ff9ffad9
zeeshell <h@hhh.hn>
2018-01-24 15:48:49 -0600

    updated for ps-spotify 1.0

--- get-songlyrics.ps1
+++ get-songlyrics.ps1
@@ -1,12 +1,12 @@
 Import-Module ps-spotify
 
 while ($true) {
-    $song =  Get-SpotifyTrackStatus -Port 4381
-         if ($oldsong.track -ne $song.track ) {
+    $song =  Get-SpotifyCurrentlyPlaying
+         if ($oldsong.track -ne $song.item ) {
             Write-Output "wee"
-            $search = Invoke-RestMethod "https://genius.com/api/search/multi?per_page=5&q=$($song.track.artist_resource.name)%20$($song.track.track_resource.name)"
+            $search = Invoke-RestMethod "https://genius.com/api/search/multi?per_page=5&q=$($song.item.name)%20$($song.item.artists.name)"
             $body = Invoke-WebRequest $search.response.sections.hits[0].result.url 
             $body.ParsedHtml.body.getElementsByClassName("lyrics") | select -ExpandProperty OuterText
         }
-    $oldsong = Get-SpotifyTrackStatus -Port 4381
+    $oldsong = Get-SpotifyCurrentlyPlaying
 }
