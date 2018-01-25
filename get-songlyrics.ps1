9580ccb11c51a29f2e4cbf76941fe274088d89cf
zeeshell <h@hhh.hn>
2018-01-24 19:34:41 -0600

    Transitioned to HTML Agility pack because of cookie warning from IE

--- get-songlyrics.ps1
+++ get-songlyrics.ps1
@@ -1,12 +1,12 @@
 Import-Module ps-spotify
-
+Add-Type -path $PSScriptRoot\HtmlAgilityPack.dll
 while ($true) {
     $song =  Get-SpotifyCurrentlyPlaying
          if ($oldsong.track -ne $song.item ) {
-            Write-Output "wee"
             $search = Invoke-RestMethod "https://genius.com/api/search/multi?per_page=5&q=$($song.item.name)%20$($song.item.artists.name)"
-            $body = Invoke-WebRequest $search.response.sections.hits[0].result.url 
-            $body.ParsedHtml.body.getElementsByClassName("lyrics") | select -ExpandProperty OuterText
+            $html = New-Object HtmlAgilityPack.HtmlWeb
+            $res = $html.Load("$($search.response.sections.hits[0].result.url)")
+            $res.DocumentNode.SelectSingleNode("//p").innertext
         }
     $oldsong = Get-SpotifyCurrentlyPlaying
 }
