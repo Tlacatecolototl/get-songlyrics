c514b62a8c24645aa42d8ff296da26486d91da4a
zeeshell <h@hhh.hn>
2018-01-26 00:03:07 -0600

    update performance

--- get-songlyrics.ps1
+++ get-songlyrics.ps1
@@ -2,11 +2,10 @@ Import-Module ps-spotify
 Add-Type -path $PSScriptRoot\HtmlAgilityPack.dll
 while ($true) {
     $song =  Get-SpotifyCurrentlyPlaying
-         if ($oldsong.track -ne $song.item ) {
+         if ((Get-SpotifyCurrentlyPlaying).item -ne $song.item ) {
             $search = Invoke-RestMethod "https://genius.com/api/search/multi?per_page=5&q=$($song.item.name)%20$($song.item.artists.name)"
             $html = New-Object HtmlAgilityPack.HtmlWeb
             $res = $html.Load("$($search.response.sections.hits[0].result.url)")
             $res.DocumentNode.SelectSingleNode("//p").innertext
         }
-    $oldsong = Get-SpotifyCurrentlyPlaying
 }
