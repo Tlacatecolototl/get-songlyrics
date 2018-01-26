629601157dd3c10802a18a2d8ff666bcaa448f1e
zeeshell <h@hhh.hn>
2018-01-26 02:55:20 -0600

    added console centering

--- get-songlyrics.ps1
+++ get-songlyrics.ps1
@@ -1,11 +1,21 @@
 Import-Module ps-spotify
+Import-Module UniversalDashboard
 Add-Type -path $PSScriptRoot\HtmlAgilityPack.dll
+
 while ($true) {
     $song =  Get-SpotifyCurrentlyPlaying
-         if ((Get-SpotifyCurrentlyPlaying).item -ne $song.item ) {
+         if ($oldsong -ne $song.item.name ) {
+            clear
             $search = Invoke-RestMethod "https://genius.com/api/search/multi?per_page=5&q=$($song.item.name)%20$($song.item.artists.name)"
             $html = New-Object HtmlAgilityPack.HtmlWeb
-            $res = $html.Load("$($search.response.sections.hits[0].result.url)")
-            $res.DocumentNode.SelectSingleNode("//p").innertext
+            $lyrics = (($html.Load("$($search.response.sections.hits[0].result.url)")).DocumentNode.SelectSingleNode("//p").innertext).split("`n")
+            $oldsong = (Get-SpotifyCurrentlyPlaying).item.name
+            foreach ($line in $lyrics) {
+                $a = $(" " * [math]::max( 0, (($host.ui.RawUI.BufferSize.Width / 2) - ($line.Length / 2))))
+                write-host -nonewline "$a"
+                write-host -nonewline "$line"
+                write-host -nonewline "`n"
+            }
         }
+        start-sleep -seconds 2
 }
